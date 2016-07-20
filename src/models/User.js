dd0efa219408397d6cce2ecb9c220175b0e98878
shermanng10 <sherman.ng10@gmail.com>
2016-07-19 22:23:11 -0400

    Add User model

---
+++ src/models/User.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose'
+import { MastermindSchema } from './Mastermind'
+
+const userSchema = mongoose.Schema({
+	userId: { type: String, required: true} ,
+	game: {type: MastermindSchema, default: MastermindSchema}
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = { User }
